001b2e19ee077fe277ff0595e2954fa6a1dcd3a5
Your Name <sumeet.sharma@berkeley.edu>
2012-07-18 22:31:07 -0700

    Pushing local finals to github for the first time

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express.createServer();
+var ejs = require('ejs')
+var fs = require('fs')
+var index = fs.readFileSync(__dirname + '/index.ejs', 'utf8');
+var loggedIn = fs.readFileSync(__dirname + '/loggedinhome.ejs', 'utf8');
+app.get('/', function (req, res) {
+    res.send(ejs.render(index,{}));
+});
+app.get('/userhome', function (req, res) {
+    res.send(ejs.render(loggedIn, {}));
+});
+app.use(express.static(__dirname));
+
+app.listen(3000);
+console.log("Server running at port 3000. Open http://localhost/3000")
