7e4ac744d9a5fdd16f59449ed761c6e7c86f6c8c
Your Name <sumeet.sharma@berkeley.edu>
2012-07-24 23:32:00 -0700

    fixed post and get confusion

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require('express')
 var app = express.createServer();
 var ejs = require('ejs')
 var fs = require('fs')
+var querystring = require('querystring')
+var httpreq = require('request')
 var index = fs.readFileSync(__dirname + '/index.ejs', 'utf8');
 var loggedIn = fs.readFileSync(__dirname + '/loggedinhome.ejs', 'utf8');
+
+
+app.get('/unsubscribefb', function (req, res) {
+    console.log("DELETING SUBSCRIPTIONS")
+    httpreq.del({
+        url : "https://graph.facebook.com/111749342300038/subscriptions",
+        body : querystring.stringify({access_token : "111749342300038|_JUG87C6egRyV6bQWOT5yxdLd9g",object : "user"}),
+    },function (error,response,body) {
+        console.log("I am del")
+        console.log(">>>>"+response.statusCode)
+    });
+});
+
+
 app.get('/', function (req, res) {
     res.send(ejs.render(index,{}));
 });
@@ -11,18 +27,21 @@ app.get('/userhome', function (req, res) {
     res.send(ejs.render(loggedIn, {}));
 });
 
+app.post('/fbnewsfeed', function (req, res) {
+    console.log("GOT POSTED!")
+    if (req.method === "POST" ) {
+        // YAY. WE'RE BEING AUTO UPDATED
+		console.log("got a POST!")
+    }
+    res.send(req.body)
+});
 app.get('/fbnewsfeed', function (req, res) {
     console.log("YAY!!!!!!!!!!!")
     if (req.method === "GET") {
         // answer Facebook's verification request
         var challenge = req.query["hub.challenge"]; //need to return the 'challenge' value back to Facebook
         console.log(req.query)
-		res.send(challenge)
-    }
-    if (req.method === "POST" ) {
-        // YAY. WE'RE BEING AUTO UPDATED
-		console.log("got a POST!")
-		console.log(req)
+        res.send(challenge)
     }
 });
 app.use(express.static(__dirname));
@@ -30,3 +49,11 @@ app.use(express.static(__dirname));
 app.listen(process.env.PORT || 3000);
 console.log("Server running on port:")
 console.log(process.env.PORT || 3000)
+
+//Initialize Facebook Subscription updates for all authenticated users
+httpreq.post({
+    url : "https://graph.facebook.com/111749342300038/subscriptions",
+    body : querystring.stringify({access_token : "111749342300038|_JUG87C6egRyV6bQWOT5yxdLd9g",object : "user",fields : "feed",callback_url : "http://powerful-caverns-1047.herokuapp.com/fbnewsfeed",verify_token : "oklol"}),
+},function (error,response,body) {
+    console.log("status "+response.statusCode)
+});
